37385d295e2b1236aad7058f94829834c9601559
koya <sereronnfox@gmail.com>
2018-05-16 12:48:27 +0000

    a

--- db/migrate/20180515111639_change_datatype_date_of_articles.rb
+++ db/migrate/20180515111639_change_datatype_date_of_articles.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeDateOfArticles < ActiveRecord::Migration[5.0]
   def change
-    change_column :articles, :date, :datetime
+    change_column :articles, :date, 'timestamptz using date::timestamptz'
   end
 end
