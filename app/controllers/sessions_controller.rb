7b7cea4767f624c0aaefe34ea1b2acc437879081
koya <sereronnfox@gmail.com>
2018-05-21 10:45:22 +0000

    aa

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,17 @@ class SessionsController < ApplicationController
     password = params[:session][:password]
     
     if login(email, password)
+      flash[:success] = 'ログインに成功しました。'
       redirect_to @user
     else
+      flash.now[:danger] = 'ログインに失敗しました。'
       render 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
+    flash[:success] = 'ログアウトしました。'
     redirect_to root_url
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :create, :update] do
     member do
-      get :myarts
+      get 'myarts', to: 'myarts'
     end
   end
+  
+  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
